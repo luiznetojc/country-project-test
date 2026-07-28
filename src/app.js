20be2f0684f8f432dddd3e444eaedc7d005015e9
Pedro Gabriel <pedro.gabriel.araujo08@gmail.com>
2026-07-28 19:39:02 -0300

    Config Inicial do projeto Paises

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+const porta = 3000
+
+
+app.listen(porta , () =>{
+    console.log("Servidor Funcionando!")
+})
+
+app.get("/", (req,res) => {
+    res.send("Api Funcionando")
+})
